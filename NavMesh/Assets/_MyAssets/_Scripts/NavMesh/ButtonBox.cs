74437d7994f37d85474013d118a7acc8b7458364
Yorman <yormanh@gmail.com>
2023-07-27 20:48:35 +0100

    NavMesh

---
+++ NavMesh/Assets/_MyAssets/_Scripts/NavMesh/ButtonBox.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class ButtonBox : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- NavMesh/Assets/_MyAssets/_Scripts/NavMesh/Follower.cs
+++ NavMesh/Assets/_MyAssets/_Scripts/NavMesh/Follower.cs
@@ -7,11 +7,13 @@ public class Follower : MonoBehaviour
 {
     NavMeshAgent _agent;
     [SerializeField] Transform _destiny;
+    [SerializeField] Transform _puntero;
  
     void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
         //_agent.SetDestination(_destiny.position);
+
     }
 
  
@@ -23,16 +25,67 @@ public class Follower : MonoBehaviour
         {
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
             Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
 
+            RaycastHit[] hits = Physics.RaycastAll(ray.origin, ray.direction, 1000);
 
-            RaycastHit hit;
-            if (Physics.Raycast(ray.origin, ray.direction, out hit, 1000))
+            //for(int i =0; i < hits.Length; i++)
+            //{
+            //    //hits[i];
+            //}
+
+            bool isRampa = false;
+            foreach (var hit in hits)
             {
-                _agent.SetDestination(hit.point);
-                Debug.Log(hit.collider.gameObject.name);
+
+
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Suelo"))
+                {
+                    if(hit.collider.name == "Rampa")
+                    {
+                        isRampa = true;
+                        _agent.SetDestination(hit.point);
+                        _puntero.position = hit.point;
+                        _puntero.rotation = Quaternion.LookRotation(hit.normal);
+
+                    }
+                    else if (isRampa == false)
+                    {
+                        _agent.SetDestination(hit.point);
+                        _puntero.position = hit.point;
+                        _puntero.rotation = Quaternion.LookRotation(hit.normal);
+                    }
+                }
+
+                ButtonBox buttonBox = hit.collider.GetComponent<ButtonBox>();
+                if (buttonBox)
+                {
+                    //buttonBox.Use();
+                }
+
+
             }
+
+
+
+            //RaycastHit hit;
+            //if (Physics.Raycast(ray.origin, ray.direction, out hit, 1000))
+            //{
+            //    //_agent.SetDestination(hit.point);
+            //    Debug.Log(hit.collider.gameObject.name);
+
+            //    ButtonBox buttonBox = hit.collider.GetComponent<ButtonBox>();
+
+            //    if (buttonBox)
+            //    {
+            //        //buttonBox.Use();
+            //    }
+            //    else
+            //    {
+            //        _agent.SetDestination(hit.point);
+            //    }
+
+            //}
         }
     }
 }
